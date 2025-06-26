d9369b1402fe87f4e321089554017d15c00dbe17
Looly <loolly@aliyun.com>
2025-06-26 17:21:35 +0800

    fix test

--- hutool-json/src/test/java/cn/hutool/json/IssueI676IT.java
+++ hutool-json/src/test/java/cn/hutool/json/IssueI676IT.java
@@ -3,18 +3,18 @@ package cn.hutool.json;
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.util.XmlUtil;
 import cn.hutool.json.xml.JSONXMLSerializer;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import static javax.xml.xpath.XPathConstants.STRING;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IssueI676IT {
 
 	@Test
 	public void parseXMLTest() {
-		final JSONObject jsonObject = JSONUtil.parseObj(ResourceUtil.readUtf8Str("issueI676IT.json"));
+		final JSONObject jsonObject = JSONUtil.parseObj(ResourceUtil.readUtf8Str("IssueI676IT.json"));
 		String xmlStr = JSONXMLSerializer.toXml(jsonObject, null, (String) null);
 		String content = String.valueOf(XmlUtil.getByXPath("/page/orderItems[1]/content", XmlUtil.readXML(xmlStr), STRING));
-		assertEquals(content, "bar1");
+		assertEquals("bar1", content);
 	}
 }

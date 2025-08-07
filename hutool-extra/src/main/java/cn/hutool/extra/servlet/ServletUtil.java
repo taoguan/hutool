a27ebb4403121356032411acdd9b0590e3895aa3
handy <handyplus@163.com>
2025-08-07 13:35:00 +0800

    getClientIP 优先获取传入的请求头信息

--- hutool-extra/src/main/java/cn/hutool/extra/servlet/ServletUtil.java
+++ hutool-extra/src/main/java/cn/hutool/extra/servlet/ServletUtil.java
@@ -215,7 +215,7 @@ public class ServletUtil {
 	public static String getClientIP(HttpServletRequest request, String... otherHeaderNames) {
 		String[] headers = {"X-Forwarded-For", "X-Real-IP", "Proxy-Client-IP", "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR"};
 		if (ArrayUtil.isNotEmpty(otherHeaderNames)) {
-			headers = ArrayUtil.addAll(headers, otherHeaderNames);
+			headers = ArrayUtil.addAll(otherHeaderNames, headers);
 		}
 
 		return getClientIPByHeader(request, headers);
--- hutool-http/src/main/java/cn/hutool/http/server/HttpServerRequest.java
+++ hutool-http/src/main/java/cn/hutool/http/server/HttpServerRequest.java
@@ -391,7 +391,7 @@ public class HttpServerRequest extends HttpServerBase {
 	public String getClientIP(String... otherHeaderNames) {
 		String[] headers = {"X-Forwarded-For", "X-Real-IP", "Proxy-Client-IP", "WL-Proxy-Client-IP", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR"};
 		if (ArrayUtil.isNotEmpty(otherHeaderNames)) {
-			headers = ArrayUtil.addAll(headers, otherHeaderNames);
+			headers = ArrayUtil.addAll(otherHeaderNames, headers);
 		}
 
 		return getClientIPByHeader(headers);

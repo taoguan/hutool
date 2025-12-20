60a91d258a2d8963fda9f14579cdb41effd7a0da
kaseketsu <755740189@qq.com>
2025-12-20 22:14:17 +0800

    fix: 修正 Models.java 文件中枚举命名大小写混用问题，如 Doubao_Seedance_1_0_lite_t2v -> DOUBAO_SEEDANCE_1_0_LITE_T2V；修复枚举值缺失问题，如 DOUBAO_LITE_128K_240828("240828") -> DOUBAO_LITE_128K_240828("doubao-lite-128k-240828")。

--- hutool-ai/src/main/java/cn/hutool/ai/Models.java
+++ hutool-ai/src/main/java/cn/hutool/ai/Models.java
@@ -109,10 +109,6 @@ public class Models {
 		DOUBAO_1_5_PRO_32K("doubao-1.5-pro-32k-250115"),
 		DOUBAO_1_5_PRO_256K("doubao-1.5-pro-256k-250115"),
 		DOUBAO_1_5_LITE_32K("doubao-1.5-lite-32k-250115"),
-		DEEPSEEK_R1("deepseek-r1-250120"),
-		DEEPSEEK_R1_DISTILL_QWEN_32B("deepseek-r1-distill-qwen-32b-250120"),
-		DEEPSEEK_R1_DISTILL_QWEN_7B("deepseek-r1-distill-qwen-7b-250120"),
-		DEEPSEEK_V3("deepseek-v3-241226"),
 		DOUBAO_PRO_4K_240515("doubao-pro-4k-240515"),
 		DOUBAO_PRO_4K_CHARACTER_240728("doubao-pro-4k-character-240728"),
 		DOUBAO_PRO_4K_FUNCTIONCALL_240615("doubao-pro-4k-functioncall-240615"),
@@ -127,7 +123,11 @@ public class Models {
 		DOUBAO_LITE_4K_PRETRAIN_CHARACTER_240516("doubao-lite-4k-pretrain-character-240516"),
 		DOUBAO_LITE_32K_240828("doubao-lite-32k-240828"),
 		DOUBAO_LITE_32K_CHARACTER_241015("doubao-lite-32k-character-241015"),
-		DOUBAO_LITE_128K_240828("240828"),
+		DOUBAO_LITE_128K_240828("doubao-lite-128k-240828"),
+		DEEPSEEK_R1("deepseek-r1-250120"),
+		DEEPSEEK_R1_DISTILL_QWEN_32B("deepseek-r1-distill-qwen-32b-250120"),
+		DEEPSEEK_R1_DISTILL_QWEN_7B("deepseek-r1-distill-qwen-7b-250120"),
+		DEEPSEEK_V3("deepseek-v3-241226"),
 		MOONSHOT_V1_8K("moonshot-v1-8k"),
 		MOONSHOT_V1_32K("moonshot-v1-32k"),
 		MOONSHOT_V1_128K("moonshot-v1-128k"),
@@ -141,10 +141,10 @@ public class Models {
 		DOUBAO_EMBEDDING_TEXT_240715("doubao-embedding-text-240715"),
 		DOUBAO_EMBEDDING_VISION("doubao-embedding-vision-241215"),
 		DOUBAO_SEEDREAM_3_0_T2I("doubao-seedream-3-0-t2i-250415"),
-		Doubao_Seedance_1_0_lite_t2v("doubao-seedance-1-0-lite-t2v-250428"),
-		Doubao_Seedance_1_0_lite_i2v("doubao-seedance-1-0-lite-i2v-250428"),
-		Wan2_1_14B_t2v("wan2-1-14b-t2v-250225"),
-		Wan2_1_14B_i2v("wan2-1-14b-i2v-250225");
+		DOUBAO_SEEDDANCE_1_0_LITE_T2V("doubao-seedance-1-0-lite-t2v-250428"),
+		DOUBAO_SEEDDANCE_1_0_lite_i2v("doubao-seedance-1-0-lite-i2v-250428"),
+		WAN2_1_14B_T2V("wan2-1-14b-t2v-250225"),
+		WAN2_1_14B_I2V("wan2-1-14b-i2v-250225");
 
 		private final String model;
 
@@ -174,7 +174,7 @@ public class Models {
 		GROK_2_IMAGE_LATEST("grok-2-image-1212"),
 		GROK_2_IMAGE("grok-2-image-1212"),
 		GROK_2_IMAGE_1212("grok-2-image-1212"),
-		grok_2_latest("grok-2-1212"),
+		GROK_2_LATEST("grok-2-1212"),
 		GROK_2("grok-2-1212"),
 		GROK_2_1212("grok-2-1212"),
 		GROK_2_VISION_1212("grok-2-vision-1212"),

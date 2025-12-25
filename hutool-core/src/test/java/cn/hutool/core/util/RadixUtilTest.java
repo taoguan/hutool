56ce21562d275d88a7c36f5527247d8313f78b6c
shad0wm00n <yangshuo.seu@gmail.com>
2025-12-25 10:45:44 +0800

    fix issue IDFPGR，修改测试方法名

--- hutool-core/src/test/java/cn/hutool/core/util/RadixUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/util/RadixUtilTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RadixUtilTest {
 	@Test
-	public void issueTest() {
+	public void issueIDFPGRTest() {
 		String radixs = "0123456789ABC"; // base 13
 		String bad = "1X3"; // 'X' 不在 radix 中
 		assertThrows(IllegalArgumentException.class, () -> {
